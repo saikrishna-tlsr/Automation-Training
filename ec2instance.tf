afe170fc9aff1fad68e51556495acf14cdff513b
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-17 16:16:43 +0530

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -8,4 +8,5 @@ resource "aws_instance" "automation" {
   }
   subnet_id = "${aws_subnet.My_VPC_Subnet.id}"
   security_groups = [ "${aws_security_group.My_VPC_Security_Group.id}" ]
+  instances = ["${aws_instance.automation.*.id}"]
 }
